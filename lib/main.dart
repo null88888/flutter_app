bbd620d95a18f65536f7d3b0d31c66e8cdf31170
zhoudingji <>
2019-08-27 18:08:32 +0800

    添加了

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,97 @@
-
-//导入了Material UI组件库
-//Material是一种标准的移动端和web端的视觉设计语言， Flutter默认提供了一套丰富的Material风格的UI组件。
-
 import 'package:flutter/material.dart';
+import 'model/post.dart';
 
-//应用入口
-void main() => runApp(MyApp());
-//(=>)符号，这是Dart中单行函数或方法的简写。
+void main() =>runApp(App());
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      home: Home(),
       theme: ThemeData(
-
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+        primaryColor: Colors.yellow,
+      )
     );
   }
 }
 
-class NewRoute extends StatelessWidget {
+ class Home extends StatelessWidget{
+
+  Widget _listItemBuilder(BuildContext context,int index){
+
+    return Container(
+      color: Colors.white,
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        children: <Widget>[
+
+          Image.network(posts[index].imageUrl),
+          SizedBox(height: 16.0),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(height: 16.0),
+        ],
+
+      ),
+    );
+//    return Text(posts[index].title);
+  }
+
   @override
-  Widget build(BuildContext context) {
+   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
       appBar: AppBar(
-        title: Text("New route"),
+        title: Text('周定基'),
+        elevation: 40.0, //阴影
       ),
-      body: Center(
-        child: Text("This is new 你的额"),
-      ),
+      body:ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: _listItemBuilder,
+      ) ,
     );
   }
-}
+ }
 
-class TipRoute extends StatelessWidget{
+  class Hello extends StatelessWidget{
 
-  TipRoute({
-     Key key,
-    @required this.text,
-  }):super(key:key);
-  final String text;
-
-  @override
-  Widget build(BuildContext context){
-
-     return Scaffold(
-       appBar: AppBar(
-         title: Text("提示"),
-       ),
-       body: Padding(
-         padding: EdgeInsets.all(18),
-         child:  Center(
-           child: Column(
-             children: <Widget>[
-               Text(text),
-               RaisedButton(
-                 onPressed: ()=>Navigator.pop(context,"我是返还值"),
-                 child: Text("返回"),
-
-               )
-             ],
+    @override
+      Widget build(BuildContext context) {
+         return Center(
+           child: Text(
+              'hello',
+            textDirection: TextDirection.ltr,
+            style: TextStyle(
+             fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+             color: Colors.black87,
+            ),
            ),
-         ),
-       ),
-     );
-  }
-}
-
-class RouterTestRoute extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: RaisedButton(
-        onPressed: () async {
-          // 打开`TipRoute`，并等待返回结果
-          var result = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) {
-                return TipRoute(
-                  // 路由参数
-                  text: "我是提示xxxx",
-                );
-              },
-            ),
-          );
-          //输出`TipRoute`路由返回结果
-          print("路由返回值: $result");
-        },
-        child: Text("打开提示页"),
-      ),
-    );
-  }
-}
-
-
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+         );
+      }
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ), //导航栏
-      body: Center(
-        child: Column(
 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have clicked the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-            FlatButton(
-              child: Text("open new route..."),
-              textColor: Colors.blue,
-              onPressed: () {
-                //导航到新路由
-                Navigator.push( context,
-                    MaterialPageRoute(builder: (context) {
-                      return TipRoute();
-                    }));
-              },
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
